2691aa8a52b38e48e4d87dbc7ba798bdef65f74d
valentinminzu <vali.minzu11@gmail.com>
2026-05-31 21:09:34 +0000

    feat: alu completat cu AND OR XOR NOT

--- alu.c
+++ alu.c
@@ -1,13 +1,13 @@
 #include "cpu.h"
 
 uint8_t alu_add(uint8_t A, uint8_t B, uint8_t *flags) {
-    if(A + B > 255) {
+    if((A + B) > 255) {
         flags[0] |= FLAG_C;
     }
     else {
         flags[0] &= ~FLAG_C;
     }
-    if(A + B == 0) {
+    if((A + B) == 0) {
         flags[0] |= FLAG_Z;
     }
     else {
@@ -17,7 +17,7 @@ uint8_t alu_add(uint8_t A, uint8_t B, uint8_t *flags) {
 }
 
 uint8_t alu_sub(uint8_t A, uint8_t B, uint8_t *flags) {
-    if(A - B == 0) {
+    if((A - B) == 0) {
         flags[0] |= FLAG_Z;
     }
     else {
@@ -33,10 +33,50 @@ uint8_t alu_sub(uint8_t A, uint8_t B, uint8_t *flags) {
 }
 
 void alu_cmp(uint8_t A, uint8_t B, uint8_t *flags) {
-    if (A - B == 0) {
+    if ((A - B) == 0) {
         flags[0] |= FLAG_Z;
     }
     else {
         flags[0] &= ~FLAG_Z;
     }
+}
+
+uint8_t alu_and(uint8_t A, uint8_t B, uint8_t *flags) {
+    if ((A & B) == 0) {
+        flags[0] |= FLAG_Z;
+    }
+    else {
+        flags[0] &= ~FLAG_Z;
+    }
+    return A & B;
+}
+
+uint8_t alu_or(uint8_t A, uint8_t B, uint8_t *flags) {
+    if ((A | B) == 0) {
+        flags[0] |= FLAG_Z;
+    }
+    else {
+        flags[0] &= ~FLAG_Z;
+    }
+    return A | B;
+}
+
+uint8_t alu_xor(uint8_t A, uint8_t B, uint8_t *flags) {
+    if ((A ^ B) == 0) {
+        flags[0] |= FLAG_Z;
+    }
+    else {
+        flags[0] &= ~FLAG_Z;
+    }
+    return A ^ B;
+}
+
+uint8_t alu_not(uint8_t A, uint8_t *flags) {
+    if (~A == 0) {
+        flags[0] |= FLAG_Z;
+    }
+    else {
+        flags[0] &= ~FLAG_Z;
+    }
+    return ~A;
 }
